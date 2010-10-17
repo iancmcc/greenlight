4ae9fa7de092d7a4f5f1fdeeae0ad7411158294a
Ian McCracken <ian.mccracken@gmail.com>
2010-10-17 13:56:57 -0500

    Added info to setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    long_description = f.read()
 
 setup(name='greenlight',
       version=version,
       description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      long_description=long_description,
+      classifiers=[],
+      keywords='gevent',
+      author='Ian McCracken',
+      author_email='ian.mccracken@gmail.com',
+      url='http://concisionandconcinnity.blogspot.com',
+      license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=['gevent'],
       entry_points="""
-      # -*- Entry points: -*-
       """,
       )
